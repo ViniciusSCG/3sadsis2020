da09b5558b2c6443b07cb234121bed03bea87d90
ViniciusSCG <57968324+ViniciusSCG@users.noreply.github.com>
2020-06-20 19:41:07 -0300

    aula20200619

--- src/main/java/aula20200619/Pessoa.java
+++ src/main/java/aula20200619/Pessoa.java
@@ -2,9 +2,9 @@ package aula20200619;
 
 
 public class Pessoa {
-	public String nome;
-	public String cpf;
-	public String dataNascimento;
+	protected String nome;
+	protected String cpf;
+	protected String dataNascimento;
 
 	public Pessoa(String nome, String cpf, String data) {
 		this.nome = nome;
